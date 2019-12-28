a414c28acdba6016e5d3561e0f1eebbadb728caf
EstebanMeza <emeza06@misena.edu.co>
2019-12-28 11:26:51 -0500

    nada que lo logro

--- public/src/js/autenticacion/autenticar.js
+++ public/src/js/autenticacion/autenticar.js
@@ -1,9 +1,68 @@
-
-function ingresarUsuario(params) {
+ class Autenticar{
+    ingresarUsuario(nombre,email,password) {
     
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(function (result) {
+        
+                result.user.updateProfile({
+                    displayName:nombre   
+                })
+                let configuracion={url:'http://127.0.0.1:5500/appTiendaAndrea/public/'}
+                result.user.sendEmailVerification()
+                                    .catch(function (error) {
+                        console.error(error)
+                                        alert(error.message,4000)
+                                    })
+                                    firebase.auth().signOut()
+                 alert(`vienvenido ${nombre} reaiza el proceso de verificar`,4000)
+                
+            })
+            .catch(function (error) {
+        
+            });
+
+}
 }
 
-function iniciarSecion(params) {
+// let $nombre =document.getElementById('nombre').value
+// let $email=document.getElementById('email').value
+// let $password=document.getElementById('password').value
+// let $btningresarDatos=document.getElementById('btningresarDatos')
+// $btningresarDatos.addEventListener('click',alert(456))
+// debugger
+// function ingresarUsuario(nombre,email,password) {
+//     firebase.auth().createUserWithEmailAndPassword(email, password)
+//         .then(result=> {
+//             result.user.UpdateProfile({
+//                displayName:nombre
+                     
+//         })
+//         let configuracion={url:'http://127.0.0.1:5500/appTiendaAndrea/public/'}
+        
+//                result.user.sendEmailVerification()
+//                     .catch(function (error) {
+//         console.error(error)
+//                         alert(error.message,4000)
+//                     })
+//                     firebase.auth().signOut()
+//  alert(`vienvenido ${nombre} reaiza el proceso de verificar`,4000)
+
+//         })
+//         .catch(function (error) {
     
-}
-var saludo="hola"
+//             console.error(error)
+//             alert(error.message,4000)
+//         });
+//     // firebase.auth().createUserWithEmailAndPassword(email,password)
+//     //     .then(result=>{
+//     //         result.user.UpdateProfile({
+//     //             displayName:nombre
+//     //         })  
+//     //         result.user.sendEmailVerification   
+//     //     }) 
+//     //     })
+// }
+
+// function iniciarSecion(params) {
+    
+// }
---
+++ public/src/js/autenticacion/controladorAutenticar.js
@@ -0,0 +1,17 @@
+// import Autenticar from 'Autenticar'
+// alert(465)
+// let $btningresarDatos=document.getElementById('btningresarDatos')
+// $btningresarDatos.addEventListener('click',ingresar)
+let autenticar = new Autenticar();
+
+function ingresar()
+{
+    console.log(autenticar)
+    console.log(autenticar.ingresarUsuario('esteban','estebanmezabetancur@gmail.com','789456'))
+//     let $nombre =document.getElementById('nombre').value
+//     let $email=document.getElementById('email').value
+//     let $password=document.getElementById('password').value
+//     alert($nombre)
+// autenticar.ingresarUsuario($nombre,$email,$password)
+}
+
